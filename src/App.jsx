ebbc5227d99debdce0b6657f60fb87e6f6acf274
yuliiariabych <riabych.yuliia@student.uzhnu.edu.ua>
2026-02-06 18:10:14 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import AddBookPage from './pages/AddBookPage';
 import AboutPage from './pages/AboutPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/home" element={<HomePage />} />
           <Route path="/add" element={<AddBookPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </Router>
